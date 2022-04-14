6d1d7efe003547a63da271097e33c113e56f9e04
Wahidul30 <wahidul505@gmail.com>
2022-04-15 00:23:48 +0600

    created json data for each dishes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
+import Breakfast from './Pages/Home/Breakfast/Breakfast';
+import Lunch from './Pages/Home/Lunch/Lunch';
+import Dinner from './Pages/Home/Dinner/Dinner';
 
 function App() {
   return (
@@ -9,7 +12,11 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='/home' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}>
+          <Route path='breakfast' element={<Breakfast />}></Route>
+          <Route path='lunch' element={<Lunch />}></Route>
+          <Route path='dinner' element={<Dinner />}></Route>
+        </Route>
       </Routes>
     </div>
   );
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
 import Banner from '../Banner/Banner';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <div className='flex gap-6 justify-center mt-6 mb-4 font-semibold text-gray-700'>
+                <NavLink to='/home/breakfast'>Breakfast</NavLink>
+                <NavLink to='/home/lunch'>Lunch</NavLink>
+                <NavLink to='/home/dinner'>Dinner</NavLink>
+            </div>
+            <div>
+                <Outlet />
+            </div>
+
         </div>
     );
 };
