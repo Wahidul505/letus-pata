aa9dbcd79234380c4cc6bd4bbd8a3d5cbc5c0669
Wahidul30 <wahidul505@gmail.com>
2022-04-14 21:55:26 +0600

    implemented the banner with search input

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import bannerBg from '../../../images/bannerbackground.png';
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='relative w-full'>
+            <img className='w-full' src={bannerBg} alt="" />
+            <div className='absolute left-0 right-0 top-0 flex justify-center items-center h-full flex-col'>
+                <h1 className='text-xl md:text-4xl mb-4 text-amber-500'>Fast, Fresh, Intent Dishes</h1>
+                <div className='bg-white flex justify-between rounded-3xl w-1/2'>
+                    <input className='w-full p-1 md:p-2 rounded-3xl' type="text" name="" placeholder='Search Your Favorite Dishes' /><button className='rounded-3xl bg-red-500 text-white px-3 py-2'>Search</button></div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {FiShoppingCart} from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='flex justify-between px-6 py-3 bg-white items-center'>
+            <h1 className='text-3xl'>Letus Pata</h1>      
+            <nav className='flex gap-6 items-center text-xl'>
+                <Link to='/cart'><FiShoppingCart /></Link>
+                <Link to='/login'>Login</Link>
+                <Link className='bg-red-500 rounded-3xl py-1 px-4 text-white' to='/signup'>Sign up</Link>
+            </nav>
         </div>
     );
 };
